af05537353190914212c24d9aa3ffd7a0dad96aa
Dana Burkart <discodancemonkey@gmail.com>
2012-04-03 22:20:46 -0400

    Everything is working reasonably well

--- safegoto.cpp
+++ safegoto.cpp
@@ -112,7 +112,9 @@ struct Vector {
 	// Return the components of this vector in the form of a point.
 	//
 	Point components() const {
-		return (Point){ magnitude * cos(direction), magnitude * sin(direction) };
+		double d = direction;
+	
+		return (Point){ magnitude * cos(d), magnitude * sin(d) };
 	}
 	
 	//
@@ -122,10 +124,24 @@ struct Vector {
 		Point cx = components() + v.components();
 		Vector vn;
 		
-		vn.direction = atan(cx.y / cx.x);
-		vn.magnitude = pow(cx.y*sin(vn.direction), -1);
-		
+		vn.direction = atan2(cx.y, cx.x);
+		vn.magnitude = pow(cx.x*cos(vn.direction), -1);
+
 		return vn;
+	}
+	
+	Vector operator-() const {
+		Point c = components();
+		c = (Point){-c.x, -c.y};
+		return (Vector){atan2(c.y, c.x), magnitude};
+	}
+	
+	int quadrant() const {
+		double dir = rtod( direction );
+		if ( dir >= 0 && dir <= 90 ) return 0;
+		if ( dir > 90 && dir <= 180 ) return 1;
+		if ( dir < 0 && dir >= -90 ) return 2;
+		return 3;
 	}
 	
 	void print() {
@@ -141,23 +157,33 @@ struct Vector {
 class Robot {
 public:
 
-	Robot( PlayerClient *c, Path p ) :
+	Robot( PlayerClient *c, Path p, bool laser ) :
 		client( c ),
 		pp( c, 0 ),
-		rp( c, 0 ),
 		path( p )
 	{
 		position = (Point){ 0.0, 0.0 };
 		velocity = (Vector){ 0.0, 0.0 };
 		currentGoal = path.begin();
-		rdata.resize(102);
+		
+		if ( laser ) {
+			rp = new RangerProxy( c, 0 );
+			rdata.resize(102);
+			bunchSize = 5;
+		} else {
+			sp = new SonarProxy( c, 0 );
+			rdata.resize(8);
+			bunchSize = 1;
+		}
+		
+		ranger = laser;
 	}
 	
 	~Robot() {}
 	
 	//
 	// Add a processing hook to this robot. The order hooks are called
-	// is fifo, so add lowest processing layers first.R
+	// is fifo, so add lowest processing layers first.
 	//
 	void AddHook( RobotHook h ) {
 		hooks.push_back( h );
@@ -167,7 +193,7 @@ public:
 	// Enter the run loop
 	//
 	int Run() {
-		while (!rp.IsValid()) client->Read();
+		while (ranger && !rp->IsValid()) client->Read();
 		
 		for(;;) {
 			RobotHookList::iterator it;
@@ -199,18 +225,29 @@ public:
 	}
 	
 	void UpdateRangeData() {
-		for ( int i = 0; i < 102; i++ ) {
+	
+		for (int i = 0; i < rdata.size(); i++ ) {
 			Vector vAvg = (Vector){0.0, 0.0};
+			double c = 0.0;
 			
-			for ( int j = 0; j < 5; j++ ) {
-				Vector vTemp = (Vector){ (3.14159 * (i * j)) / (510), 
-										 rp[ 30 + (i * j) ] };
-										 
-				if ( j == 0 ) vAvg = vTemp;
-				else vAvg = vAvg + vTemp;
+			for ( int j = 0; j < bunchSize; j++ ) {
+				double sample = GetRangeSample((i * bunchSize) + j);
+				
+				Vector vTemp = (Vector){ ((3.14159 * ((i * bunchSize) + j)) / GetSampleSize()), 
+										 sample };
+				 
+				if ( sample > 0.0 ) {
+					if ( j == 0 ) vAvg = vTemp;
+					else vAvg.direction += vTemp.direction, vAvg.magnitude += vTemp.magnitude;
+					c = c + 1.0;
+				}
 			}
 			
-			vAvg.magnitude  /= 5.0;
+			if ( c > 1.0 ) {
+				vAvg.magnitude /= c;
+				vAvg.direction /= c;
+			}
+			
 			rdata[i] = vAvg;
 		}
 	}
@@ -219,8 +256,24 @@ public:
 		return &rdata;
 	}
 	
-	Vector GetVelocity() {
-		return velocity;
+	const Vector *GetVelocity() {
+		return &velocity;
+	}
+	
+	double GetRangeSample(int n) {
+		if ( ranger ) {
+			return (*rp)[30 + n];
+		} else {
+			return (*sp)[GetSampleSize() - n];
+		}
+	}
+	
+	int GetSampleSize() {
+		if ( ranger ) {
+			return 510;
+		} else {
+			return 8;
+		}
 	}
 
 protected:
@@ -229,19 +282,25 @@ protected:
 	Vector		velocity;
 	Path		path;
 	RangeData	rdata;
+	int			bunchSize;
 	
 private:
 	
 	PlayerClient	*client;
 	Position2dProxy	pp;
-	RangerProxy		rp;
+	RangerProxy		*rp;
+	SonarProxy		*sp;
+	bool 			ranger;
 	
 	RobotHookList 	hooks;
 	Path::iterator	currentGoal;
 	
 };
 
-bool turning = true;
+bool turning = false;
+bool adjusting = true;
+
+double k_1 = 1.0, k_2 = 1.0;
 
 int goToPoint(Robot *robot, Point *at, Vector *velocity) {
 	Point dest = robot->GetGoal();
@@ -259,54 +318,87 @@ int goToPoint(Robot *robot, Point *at, Vector *velocity) {
 	dtheta = theta - velocity->direction;
 	
 	// Calculate our acceleration
-	accel = 1.0*((*at) - dest) + 1.0*(0.0 - velocity->magnitude);
+	accel = k_1*((*at) - dest) + k_2*(0.0 - velocity->magnitude);
 	
-	if ( turning && fabs(dtheta) > .02 ) {
-		velocity->direction = dtheta;
+	if (fabs(dtheta) <= .02) turning = false; 
+	
+	if ( adjusting && fabs(dtheta) > .02 ) {
+		velocity->direction = theta;
 		velocity->magnitude = 0.0;
 	}
 	
 	// If we haven't started moving yet, OR we have and are still far enough
 	// away, and we haven't overshot.
 	else if ( !velocity->magnitude || velocity->magnitude == 1 || ((*at) - dest > .02 && accel <= 0.0) ) {
-		velocity->direction = 0.0;
-		velocity->magnitude = (1 < velocity->magnitude + accel) ? 
+		velocity->direction = (turning) ? theta : robot->GetVelocity()->direction;
+		velocity->magnitude = ( 1 < velocity->magnitude + accel) ? 
 			1 : velocity->magnitude + accel;
-		turning = false;
+		adjusting = false;
 	} 
 	
 	// We've gotten to the point, so stop the robot, get the next point, and
 	// reset 'turning'
 	else {
-		velocity->direction = 0.0;
-		velocity->magnitude = 0.0;
+		velocity->direction = robot->GetVelocity()->direction;
 		robot->GoalAchieved();
-		turning = true;
+		adjusting = true;
 	}
 }
 
 int obstacleAvoidance(Robot *robot, Point *at, Vector *velocity) {
 	RangeData *rdata = robot->GetRangeData();
 	RangeData::iterator it;
-	Vector rForce = (Vector){ 0.0, 0.0 };
-	Vector currentVelocity = robot->GetVelocity();
+	Vector rForce = (Vector){ 0.0, 0.0 }, force;
+	const Vector currentVelocity = *robot->GetVelocity();
 	
+	std::cout << "====== Obstacles ======" << std::endl;
 	// Calculate a repelling force (rForce) to push our robot away
 	// from obstacles.
 	for ( it = rdata->begin(); it < rdata->end(); it++ ) {
+		it->print();
 		// Throw out data we don't care about
-		if ( it->magnitude == it->magnitude && it->magnitude > 0 ) {
-			it->magnitude = 1.0 / (it->magnitude * 2);
-			it->direction *= -1.0;
+		if ( it->magnitude == it->magnitude && it->magnitude > 0.0 && 
+			 it->magnitude < 1.0 && it->direction > dtor(45) &&
+			 it->direction < dtor(135) ) {
 			
 			if (rForce.magnitude == 0.0) rForce = *it;
 			else rForce = rForce + (*it);
 		}
 	}
 	
-	if ( velocity->magnitude > 0 ) {
-		velocity->direction = (currentVelocity + rForce).direction - currentVelocity.direction;
+	std::cout << "rForce: ";
+	rForce.print();
+	
+	rForce.direction -= (.5 * 3.14159);
+	rForce.direction += robot->GetVelocity()->direction;
+	
+	force = (-rForce) + *robot->GetVelocity();
+	
+	std::cout << "force: ";
+	force.print();
+	
+	if ( fabs(force.direction - robot->GetVelocity()->direction) > .02 && 
+		 velocity->magnitude > 0.0 && rForce.magnitude > 0.0) {
+		velocity->direction = force.direction;
+		
+		
+		turning = true;
 	}
+	
+	std::cout << "resulting vector: ";
+	velocity->print();
+}
+
+int convertToTurnrate(Robot *robot, Point *at, Vector *velocity) {
+	double dtheta = 0.0;
+	
+	// Calculate our delta theta
+	dtheta = velocity->direction - robot->GetVelocity()->direction;
+	
+	// Set our velocity's direction to a turnrate
+	velocity->direction = dtheta;
+	
+	velocity->print();
 }
 
 int main( int argc, char *argv[] ) {
@@ -338,10 +430,11 @@ int main( int argc, char *argv[] ) {
 		client = new PlayerClient( "localhost", 6665 );
 	}
 	
-	Robot robot( client, path );
+	Robot robot( client, path, (strcmp( argv[1], "laser" ) == 0) );
 	
 	robot.AddHook( &goToPoint );
 	robot.AddHook( &obstacleAvoidance );
+	robot.AddHook( &convertToTurnrate );
 	
 	robot.Run();
 	
