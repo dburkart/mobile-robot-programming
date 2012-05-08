e7d5ef244028c31f7450356f3e94915c50849a60
Dana Burkart <discodancemonkey@gmail.com>
2012-05-08 18:49:15 -0400

    Forgot to commit Planning

---
+++ planning.cpp
@@ -0,0 +1,6 @@
+#include "planning.h"
+
+Path PlanPath( Point src, Point dest ) {
+
+	return Path();
+}
---
+++ planning.h
@@ -0,0 +1,9 @@
+#ifndef __PLANNING_H__
+#define __PLANNING_H__
+
+#include "physics.h"
+
+Path PlanPath( Point src, Point dest );
+
+#endif
+
